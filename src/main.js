a1982acb7696bc31493b017ca71af4d63ecb19ad
fanfan <28298253734@qq.com>
2022-07-01 09:20:40 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import "@/mobile/flexible"
+import "@/styles/reset.css"
+import router from '@/router'
+
+import { Tabbar, TabbarItem, NavBar } from 'vant';
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(NavBar);
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
